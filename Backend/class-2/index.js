7f12e0750822444e7e6432fa10220017aaf1ddf2
Pramodjena <pramoodjena@gmail.com>
2024-05-09 12:03:01 +0530

    new file added

--- Backend/class-2/index.js
+++ Backend/class-2/index.js
@@ -3,20 +3,24 @@ const fs = require("fs");
 const math = require("./data");
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/write") {
-    fs.writeFileSync("text.txt", "Hey This is Pramod");
+  if (req.url === "/") {
+    res.end("Home Page");
+  } else if (req.url === "/write") {
+    fs.writeFileSync("text.txt", "Hey This is Pramod \n");
     res.end("File created sucessfully");
   } else if (req.url === "/read") {
     const readText = fs.readFileSync("./text.txt", "utf-8");
     res.end(readText);
   } else if (req.url === "/append") {
-    fs.appendFileSync("./text.txt", "\n Hey welcome to web bocket \n");
+    fs.appendFileSync("./text.txt", "Hey welcome to web bocket \n");
     const readText = fs.readFileSync("./text.txt", "utf-8");
     res.end(readText);
   } else if (req.url === "/math") {
-    const sum = math.sum(100, 25);
-    const subtract = math.subtract(100, 25);
-    const result = `The sum value is: ${sum} and the subtract value is:${subtract}`;
+    const sum = math.sum(75, 25);
+    const subtract = math.subtract(55, 25);
+    const multiplication = math.multiplication(15, 25);
+    const division = math.division(75, 15);
+    const result = `The sum value is: ${sum}, the subtract value is:${subtract}, the multiplication value is:${multiplication}, The division value is: ${division}`;
     fs.appendFileSync("./text.txt", result.toString() + "\n");
     const readText = fs.readFileSync("./text.txt", "utf-8");
     res.end(readText);
